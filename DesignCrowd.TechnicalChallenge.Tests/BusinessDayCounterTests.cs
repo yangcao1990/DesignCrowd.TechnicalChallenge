db46cf7362d653b42d473f2594c0dfc128d4f8d8
Yang Cao <yangcao1990@gmail.com>
2024-10-10 15:43:44 +1100

    calculate weekdays between two given dates

--- DesignCrowd.TechnicalChallenge.Tests/BusinessDayCounterTests.cs
+++ DesignCrowd.TechnicalChallenge.Tests/BusinessDayCounterTests.cs
@@ -2,5 +2,67 @@ namespace DesignCrowd.TechnicalChallenge.Tests
 {
     public class BusinessDayCounterTests
     {
+        private readonly BusinessDayCounter _businessDayCounter;
+
+        public BusinessDayCounterTests()
+        {
+            _businessDayCounter = new BusinessDayCounter();
+        }
+
+        [Fact]
+        public void WeekdaysBetweenTwoDates_FirstDateSameAsSecondDate_ReturnsZero()
+        {
+            var startDate = DateTime.Now;
+            var count = _businessDayCounter.WeekdaysBetweenTwoDates(startDate, startDate);
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void WeekdaysBetweenTwoDates_FirstDateAfterSecondDate_ReturnsZero()
+        {
+            var startDate = new DateTime(2013, 10, 7);
+            var endDate = new DateTime(2013, 10, 5);
+            var count = _businessDayCounter.WeekdaysBetweenTwoDates(startDate, endDate);
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void WeekdaysBetweenTwoDates_FirstDateOneDayBeforeSecondDate_ReturnsZero()
+        {
+            var startDate = DateTime.Now;
+            var endDate = startDate.AddDays(1);
+            var count = _businessDayCounter.WeekdaysBetweenTwoDates(startDate, endDate);
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void WeekdaysBetweenTwoDates_FirstDateSameWeekAsSecondDate_ReturnsCorrectCount()
+        {
+            var startDate = new DateTime(2013, 10, 7);
+            var endDate = new DateTime(2013, 10, 9);
+            var count = _businessDayCounter.WeekdaysBetweenTwoDates(startDate, endDate);
+
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public void WeekdaysBetweenTwoDates_FirstDateDifferentWeekFromSecondDate_ReturnsCorrectCount()
+        {
+            var intervals = new List<(DateTime firstDate, DateTime endDate)>()
+            {
+                new(new DateTime(2013, 10, 5), new DateTime(2013, 10, 14)),
+                new(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)),
+            };
+
+            var counts = intervals.Select(k => _businessDayCounter.WeekdaysBetweenTwoDates(k.firstDate, k.endDate)).ToList();
+
+            Assert.Equal(2, counts.Count);
+            Assert.Equal(5, counts[0]);
+            Assert.Equal(61, counts[1]);
+        }
+
     }
 }
--- DesignCrowd.TechnicalChallenge/BusinessDayCounter.cs
+++ DesignCrowd.TechnicalChallenge/BusinessDayCounter.cs
@@ -2,9 +2,49 @@
 {
     public class BusinessDayCounter
     {
+        private static bool IsWeekday(DateTime date)
+        {
+            return date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday;
+        }
+
+        /// <summary>
+        /// Calculates the number of weekdays between firstDate and secondDate, excluding firstDate and secondDate
+        /// <list type="bullet">
+        ///     <item>
+        ///         <description>Weekdays are Monday, Tuesday, Wednesday, Thursday, Friday.</description>
+        ///     </item>
+        ///     <item>
+        ///         <description>The returned count <b>does not</b> include either firstDate or secondDate.</description>
+        ///     </item>
+        /// </list>
+        /// </summary>
+        /// <param name="firstDate">The date to start from, excl. from the count</param>
+        /// <param name="secondDate">The date to stop, excl. from the count</param>
+        /// <returns>
+        /// Number of weekdays between firstDate and secondDate, excluding firstDate and secondDate;
+        /// returns 0 if secondDate is equal to or before firstDate
+        /// </returns>
         public int WeekdaysBetweenTwoDates(DateTime firstDate, DateTime secondDate)
         {
-            throw new NotImplementedException();
+            if (firstDate >= secondDate)
+            {
+                return 0;
+            }
+
+            var startDate = firstDate.AddDays(1).Date;
+            var endDate = secondDate.AddDays(-1).Date;
+
+            var weekdays = 0;
+
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if (IsWeekday(date))
+                {
+                    weekdays++;
+                }
+            }
+
+            return weekdays;
         }
 
         public int BusinessDaysBetweenTwoDates(DateTime firsDate, DateTime secondDate, IList<DateTime> publicHolidays)
