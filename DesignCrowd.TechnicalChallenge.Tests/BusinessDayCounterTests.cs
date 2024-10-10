7bbc52efe01cbd44e74184e49421b567768a1c82
Yang Cao <yangcao1990@gmail.com>
2024-10-10 16:44:13 +1100

    calculate business days between two given dates, comparing with a list holiday dates

--- DesignCrowd.TechnicalChallenge.Tests/BusinessDayCounterTests.cs
+++ DesignCrowd.TechnicalChallenge.Tests/BusinessDayCounterTests.cs
@@ -64,5 +64,110 @@ namespace DesignCrowd.TechnicalChallenge.Tests
             Assert.Equal(61, counts[1]);
         }
 
+        [Fact]
+        public void BusinessDaysBetweenTwoDates_FirstDateSameAsSecondDate_ReturnsZero()
+        {
+            var startDate = DateTime.Now;
+            var count = _businessDayCounter.BusinessDaysBetweenTwoDates(startDate, startDate, null);
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void BusinessDaysBetweenTwoDates_FirstDateAfterSecondDate_ReturnsZero()
+        {
+            var startDate = new DateTime(2013, 10, 7);
+            var endDate = new DateTime(2013, 10, 5);
+            var count = _businessDayCounter.BusinessDaysBetweenTwoDates(startDate, endDate, null);
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void BusinessDaysBetweenTwoDates_FirstDateOneDayBeforeSecondDate_ReturnsZero()
+        {
+            var startDate = DateTime.Now;
+            var endDate = startDate.AddDays(1);
+            var count = _businessDayCounter.BusinessDaysBetweenTwoDates(startDate, endDate, null);
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public void BusinessDaysBetweenTwoDates_FirstDateSameWeekAsSecondDateWithoutPublicHolidays_ReturnsCorrectCount()
+        {
+            var startDate = new DateTime(2013, 10, 7);
+            var endDate = new DateTime(2013, 10, 9);
+            var count = _businessDayCounter.BusinessDaysBetweenTwoDates(startDate, endDate, null);
+
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public void BusinessDaysBetweenTwoDates_FirstDateDifferentWeekFromSecondDateWithoutPublicHolidays_ReturnsCorrectCount()
+        {
+            var intervals = new List<(DateTime firstDate, DateTime endDate)>()
+            {
+                new(new DateTime(2013, 10, 5), new DateTime(2013, 10, 14)),
+                new(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)),
+            };
+
+            var counts = intervals.Select(k => _businessDayCounter.BusinessDaysBetweenTwoDates(k.firstDate, k.endDate, null)).ToList();
+
+            Assert.Equal(2, counts.Count);
+            Assert.Equal(5, counts[0]);
+            Assert.Equal(61, counts[1]);
+        }
+
+        [Fact]
+        public void BusinessDaysBetweenTwoDates_FirstDateBeforeSecondDateWithPublicHolidayDateOnly_ReturnsCorrectCount()
+        {
+            var intervals = new List<(DateTime firstDate, DateTime endDate)>()
+            {
+                new(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)),
+                new(new DateTime(2013, 12, 24), new DateTime(2013, 12, 27)),
+                new(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)),
+            };
+
+            var publicHolidays = new List<DateTime>()
+            {
+                new(2013, 12, 25),
+                new(2013, 12, 26),
+                new(2014, 1, 1),
+            };
+
+            var counts = intervals.Select(k => _businessDayCounter.BusinessDaysBetweenTwoDates(k.firstDate, k.endDate, publicHolidays)).ToList();
+
+            Assert.Equal(3, counts.Count);
+            Assert.Equal(1, counts[0]);
+            Assert.Equal(0, counts[1]);
+            Assert.Equal(59, counts[2]);
+        }
+
+        [Fact]
+        public void BusinessDaysBetweenTwoDates_FirstDateBeforeSecondDateWithPublicHolidayDateTime_ReturnsCorrectCount()
+        {
+            var intervals = new List<(DateTime firstDate, DateTime endDate)>()
+            {
+                new(new DateTime(2013, 10, 7), new DateTime(2013, 10, 9)),
+                new(new DateTime(2013, 12, 24), new DateTime(2013, 12, 27)),
+                new(new DateTime(2013, 10, 7), new DateTime(2014, 1, 1)),
+            };
+
+            var publicHolidays = new List<DateTime>()
+            {
+                new(2013, 12, 25, 12, 0, 0),
+                new(2013, 12, 26, 9, 0, 0),
+                new(2014, 1, 1, 18, 0, 0),
+            };
+
+            var counts = intervals.Select(k => _businessDayCounter.BusinessDaysBetweenTwoDates(k.firstDate, k.endDate, publicHolidays)).ToList();
+
+            Assert.Equal(3, counts.Count);
+            Assert.Equal(1, counts[0]);
+            Assert.Equal(0, counts[1]);
+            Assert.Equal(59, counts[2]);
+        }
+
     }
 }
--- DesignCrowd.TechnicalChallenge/BusinessDayCounter.cs
+++ DesignCrowd.TechnicalChallenge/BusinessDayCounter.cs
@@ -47,9 +47,60 @@
             return weekdays;
         }
 
-        public int BusinessDaysBetweenTwoDates(DateTime firsDate, DateTime secondDate, IList<DateTime> publicHolidays)
+        /// <summary>
+        /// Calculates the number of business days between firstDate and secondDate, excluding firstDate and secondDate
+        /// <list type="bullet">
+        ///     <item>
+        ///         <description>
+        ///             Weekdays are Monday, Tuesday, Wednesday, Thursday, Friday, but excluding any dates which appear
+        ///             in the <see cref="publicHolidays"/>
+        ///         </description>
+        ///     </item>
+        ///     <item>
+        ///         <description>The returned count <b>does not</b> include either firstDate or secondDate.</description>
+        ///     </item>
+        /// </list>
+        /// </summary>
+        /// <param name="firstDate">The date to start from, excl. from the count</param>
+        /// <param name="secondDate">The date to stop, excl. from the count</param>
+        /// <param name="publicHolidays">A list of dates indicating public holidays</param>
+        /// <returns>
+        /// Number of business days between firstDate and secondDate, excluding firstDate and secondDate;
+        /// returns 0 if secondDate is equal to or before firstDate
+        /// </returns>
+        public int BusinessDaysBetweenTwoDates(DateTime firstDate, DateTime secondDate, IList<DateTime> publicHolidays)
         {
-            throw new NotImplementedException();
+            bool IsPublicHoliday(DateTime date)
+            {
+                if (publicHolidays == null)
+                {
+                    return false;
+                }
+
+                return publicHolidays.Any(holiday => holiday.Date == date.Date);
+            }
+
+            if (firstDate >= secondDate)
+            {
+                return 0;
+            }
+
+            var startDate = firstDate.AddDays(1).Date;
+            var endDate = secondDate.AddDays(-1).Date;
+
+            var businessDays = 0;
+
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if (IsWeekday(date) && !IsPublicHoliday(date))
+                {
+                    businessDays++;
+                }
+            }
+
+            return businessDays;
         }
+
+
     }
 }
